6f836c7ca47f4640261fd6e6f2081fe6911f3b22
AhmedAbdelazez27 <ahmed.abdelazeez@jrm.com.eg>
2025-06-28 10:47:55 +0300

    role list and paginations

--- src/app/features/roles/pages/roles-list.component.ts
+++ src/app/features/roles/pages/roles-list.component.ts
@@ -12,7 +12,8 @@ export class RolesListComponent {
   roles: any[] = []; 
   totalCount: number = 0;  
   currentPage: number = 1;
-  itemsPerPage: number = 10; 
+  itemsPerPage: number = 2;
+  pages: number[] = [];  
 
   constructor(private roleService: RoleService) { }
 
@@ -28,16 +29,29 @@ export class RolesListComponent {
       (data: any) => {
         this.roles = data.data; 
         this.totalCount = data.totalCount; 
+        this.calculatePages();
       },
       (error) => {
         console.error('Error fetching roles:', error);
       }
     );
   }
+  calculatePages(): void {
+    const totalPages = Math.ceil(this.totalCount / this.itemsPerPage);
+    this.pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+  }
 
-  changePage(page: number): void {
-    this.currentPage = page;
-    this.getRoles(page); 
+
+  changePage(event: any): void {
+    console.log(event);
+    console.log(typeof event);
+    
+    
+    if (event < 1) event = 1;  
+    if (event > this.pages.length) event = this.pages.length; 
+
+    this.currentPage = event; 
+    this.getRoles(event); 
 
 }
 }
